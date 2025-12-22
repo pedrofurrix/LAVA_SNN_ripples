1cf86327100399a76bdd75a3e221cc599646871c
Pedro Furrix <pedrofelixalves@gmail.com>
2025-12-22 17:18:45 +0000

    abc

--- snnTorch/generalization_madrid/batchrun.py
+++ snnTorch/generalization_madrid/batchrun.py
@@ -18,15 +18,24 @@ if __name__ == "__main__":
 
     data_path=r"C:\Madrid_tests"
     # sessions=lists_sessions.annotated_sessions
+
+    # Original Sessions 
     # session_set={"2025-09-22_17-55-26", #R
     #              "2025-09-23_15-50-26", #R
     #              "2025-09-24_10-24-40", #R
     #              "2025-09-24_14-22-55", #H
     #              "2025-09-24_15-13-10", #H
     #              "2025-09-25_16-41-14"} #R
-    session_set={"2025-09-24_16-29-07", #R   
-            "2025-09-24_17-38-17",} #R 
-    
+
+    # Extra
+#     session_set={"2025-09-24_16-29-07", #R   
+#             "2025-09-24_17-38-17",} #R 
+    session_set={ "2025-09-22_17-42-27", 
+                 "2025-09-23_16-17-52", 
+                 "2025-09-24_11-34-51",
+                 "2025-09-25_11-21-53",
+                 "2025-09-25_12-52-22",}
+
     channel_sessions=lists_sessions.channel_sessions
     run_inference(
             prefix,
