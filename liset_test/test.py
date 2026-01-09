a89251a868d44861d23d934732ecdd91ab0f4d68
Pedro Furrix <pedrofelixalves@gmail.com>
2026-01-09 19:14:30 +0000

    check_model

---
+++ liset_test/test.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import pickle as pkl
+curr_dir=os.path.dirname(os.path.abspath(__file__))
+par_dir=os.path.dirname(curr_dir)
+sys.path.insert(0, par_dir)
+
+from liset_tk.load_data import *
+from liset_tk.read_data import *
+import liset_tk.lists_sessions as lists_sessions
+from liset_test.process_signal import *
+from liset_tk.format_predictions import *
+
+import tensorflow.keras.backend as K
+import tensorflow.keras as kr
+
+model = kr.models.load_model(os.path.join(curr_dir, "model"), compile=False)
+# model.compile(loss="binary_crossentropy", optimizer=optimizer)
+model.summary()
